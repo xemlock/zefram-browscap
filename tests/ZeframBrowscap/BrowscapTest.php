f66c9da39f93dceaabe7f00c90ce0f69c3eaaeee
xemlock <xemlock@gmail.com>
2015-05-05 11:40:30 +0200

    added tests

---
+++ tests/ZeframBrowscap/BrowscapTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class ZeframBrowscap_BrowscapTest extends PHPUnit_Framework_TestCase
+{
+    public function testResult()
+    {
+        $browscap = new ZeframBrowscap_Browscap();
+        $browscap->setDatasetType('default');
+        $browscap->setAutoUpdate(true);
+
+        $result = $browscap->getBrowser('Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0');
+        $this->assertInstanceOf('stdClass', $result);
+
+        $this->assertEquals('36.0', $result->version);
+
+        if ($browscap->getDatasetType() !== 'small') {
+            $this->assertEquals(36, $result->majorver);
+        }
+
+        $this->assertFalse($result->ismobiledevice);
+
+        $result = $browscap->getBrowser('Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0', true);
+        $this->assertInternalType('array', $result);
+    }
+}
